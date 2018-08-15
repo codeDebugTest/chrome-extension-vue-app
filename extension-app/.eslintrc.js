a9df1806e4208478f23c374954b23417b34ee577
liuye11 <liuye11@baidu.com>
2018-08-15 23:32:59 +0800

    feat: eslint config

--- extension-app/.eslintrc.js
+++ extension-app/.eslintrc.js
@@ -1,29 +1,46 @@
-// https://eslint.org/docs/user-guide/configuring
+// http://eslint.org/docs/user-guide/configuring
 
 module.exports = {
   root: true,
+  parser: 'babel-eslint',
   parserOptions: {
-    parser: 'babel-eslint'
+    sourceType: 'module'
   },
   env: {
     browser: true,
   },
-  extends: [
-    // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
-    // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    'plugin:vue/essential', 
-    // https://github.com/standard/standard/blob/master/docs/RULES-en.md
-    'standard'
-  ],
+  // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
+  extends: 'standard',
   // required to lint *.vue files
   plugins: [
-    'vue'
+    'vue', 'html'
   ],
   // add your custom rules here
-  rules: {
+  'rules': {
+    // allow paren-less arrow functions
+    'arrow-parens': 0,
     // allow async-await
-    'generator-star-spacing': 'off',
+    'generator-star-spacing': 0,
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    // fix unused var error for JSX custom tags
+    'vue/jsx-uses-vars': 2,
+    'no-throw-literal': 0,
+
+    'semi': ['error', 'always'],
+    'indent': ['error', 4, {SwitchCase: 1}],
+    'space-before-function-paren': ['error', {
+        anonymous: 'always',
+        named: 'never',
+        asyncArrow: 'always'
+    }],
+    'brace-style': ['error', 'stroustrup'],
+    'operator-linebreak': ['error', 'before'],
+    'eol-last': 'error',
+    'no-new': 0,
+    'no-else-return': 0,
+    'padded-blocks': 0,
+    'no-fallthrough': 'off',
+    'default-case': 'error'
   }
 }
