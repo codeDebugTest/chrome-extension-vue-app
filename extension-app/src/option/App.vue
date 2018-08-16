5ca2d33fd0c4518e42af96c5d4e5aa87d765e775
liuye11 <liuye11@baidu.com>
2018-08-16 23:05:20 +0800

    feat: option page

---
+++ extension-app/src/option/App.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="option">
+        <h2>option page</h2>
+    </div>
+</template>
+<<script>
+import Model from './AppModel';
+
+export default {
+    name: 'app',
+    props: {
+        store: {
+            type: Object,
+            default() {
+                return new model();
+            }
+        }
+    },
+    created() {
+        this.store.init();
+    }
+}
+</script>
+<style lang="less">
+.option {
+    h2 {
+        padding: 10px;
+        margin: 10px 0 20px 0;
+        border-top: 1px solid #fff;
+        border-bottom: 1px solid #fff;
+        box-shadow: 0 0 3px 0 #ccc;
+        background-color: #eee;
+    }
+}
+</style>
