a66d8369d131da725ac5e9106c3608ad84e17c6a
liuye11 <liuye11@baidu.com>
2018-08-19 23:15:34 +0800

    feat: prod build script

--- extension-app/build/build.js
+++ extension-app/build/build.js
@@ -16,7 +16,8 @@ spinner.start()
 
 rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
     if (err) throw err
-    webpack(webpackConfig, (err, stats) => {
+
+    config.build.entries.map(entry => webpack(webpackConfig(entry), (err, stats) => {
         spinner.stop()
         if (err) throw err
         process.stdout.write(stats.toString({
@@ -37,5 +38,5 @@ rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
             '  Tip: built files are meant to be served over an HTTP server.\n' +
             '  Opening index.html over file:// won\'t work.\n'
         ))
-    })
+    }))
 })
