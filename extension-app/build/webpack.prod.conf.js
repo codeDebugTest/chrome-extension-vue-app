5a4a4d597638b5801e26753a477a29b0d4850260
liuye11 <liuye11@baidu.com>
2018-08-20 00:11:48 +0800

    fix: prod build remove copy static file

--- extension-app/build/webpack.prod.conf.js
+++ extension-app/build/webpack.prod.conf.js
@@ -116,14 +116,7 @@ function webpackConfig(entry) {
                 async: 'vendor-async',
                 children: true,
                 minChunks: 3
-            }),
-    
-            // copy custom static assets
-            new CopyWebpackPlugin([{
-                from: path.resolve(__dirname, '../static'),
-                to: config.build.assetsSubDirectory,
-                ignore: ['.*']
-            }])
+            })
         ]
     })
 
