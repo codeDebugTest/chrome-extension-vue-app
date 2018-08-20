dabc6dad2c76436ffd39a2df5ae8762ce9537708
liuye11 <liuye11@baidu.com>
2018-08-20 23:02:45 +0800

    feat: bg/cs script build

---
+++ extension-app/build/webpack.prod_com.conf.js
@@ -0,0 +1,58 @@
+'use strict'
+const path = require('path')
+const utils = require('./utils')
+const webpack = require('webpack')
+const config = require('../config')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const env = require('../config/prod.env')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+
+const webpackConfig = {
+    entry: {
+        'contentScript/cs': './src/contentScript/cs.js',
+        'contentScript/runtime': './src/contentScript/runtime.js',
+        'background/bg': './src/background/bg.js'
+    },
+    output: {
+        path: config.build.assetsRoot,
+        filename: './[name].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js)$/,
+                loader: 'eslint-loader',
+                enforce: 'pre',
+                include: [path.resolve(__dirname, '../src')],
+                options: {
+                  formatter: require('eslint-friendly-formatter'),
+                  emitWarning: !config.dev.showEslintErrorsInOverlay,
+                  emitError: true
+                }
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                include: [path.resolve(__dirname, '../src')]
+            }
+        ]
+    },
+    devtool: false,
+    plugins: [
+        // http://vuejs.github.io/vue-loader/en/workflow/production.html
+        new webpack.DefinePlugin({
+            'process.env': env
+        }),
+        new UglifyJsPlugin({
+            uglifyOptions: {
+              compress: {
+                warnings: false
+              }
+            },
+            sourceMap: false,
+            parallel: true
+        })
+    ]
+}
+
+module.exports = webpackConfig
