72987550ac364fd16a0ef0bc807d9d86ce9e494c
liuye11 <liuye11@baidu.com>
2018-08-21 21:59:21 +0800

    fix: build script

--- extension-app/build/build.js
+++ extension-app/build/build.js
@@ -10,6 +10,10 @@ const chalk = require('chalk')
 const webpack = require('webpack')
 const config = require('../config')
 const webpackConfig = require('./webpack.prod.conf')
+const webpackProdCommonConfig = require('./webpack.prod_com.conf')
+
+const buildTasks = config.build.entries.map(entry => webpackConfig(entry));
+buildTasks.push(webpackProdCommonConfig);
 
 const spinner = ora('building for production...')
 spinner.start()
@@ -17,26 +21,38 @@ spinner.start()
 rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
     if (err) throw err
 
-    config.build.entries.map(entry => webpack(webpackConfig(entry), (err, stats) => {
+    Promise.all(buildTasks.map(task => {
+        return new Promise((resolve, reject) => {
+            webpack(task, (err, stats) => {
+                if (err) throw err
+                process.stdout.write(stats.toString({
+                    colors: true,
+                    modules: false,
+                    children: false, // If you are using ts-loader, setting this to true will make TypeScript errors show up during build.
+                    chunks: false,
+                    chunkModules: false
+                }) + '\n\n')
+        
+                if (stats.hasErrors()) {
+                    console.log(chalk.red('  Build failed with errors.\n'))
+                    process.exit(1)
+                    reject()
+                }
+                resolve()
+            })
+        })
+    }))
+    .then(() => {
         spinner.stop()
-        if (err) throw err
-        process.stdout.write(stats.toString({
-            colors: true,
-            modules: false,
-            children: false, // If you are using ts-loader, setting this to true will make TypeScript errors show up during build.
-            chunks: false,
-            chunkModules: false
-        }) + '\n\n')
-
-        if (stats.hasErrors()) {
-            console.log(chalk.red('  Build failed with errors.\n'))
-            process.exit(1)
-        }
-
         console.log(chalk.cyan('  Build complete.\n'))
         console.log(chalk.yellow(
             '  Tip: built files are meant to be served over an HTTP server.\n' +
             '  Opening index.html over file:// won\'t work.\n'
         ))
-    }))
+    })
+    .catch(err => {
+        spinner.stop()
+        console.log(chalk.cyan('  Build failed.\n'))
+    })
+
 })
